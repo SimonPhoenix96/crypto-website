43e5db39a4e7b092610797d406dbbac447df5cfc
git <git@github.com>
2021-08-25 22:05:14 +0200

    add router set up for wallet page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WalletComponent } from "./wallet/wallet.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'wallet', component: WalletComponent}
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
